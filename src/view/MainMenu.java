452a9b21f53dd484e55a185280b62215c111278a
Christian Dave Janiola <janiolaChrisJ07@gmail.com>
2024-02-08 16:13:54 +0800

    Added Label and buttons to Main Menu GUI

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -1,8 +1,11 @@
 
 package view;
 
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * Main menu for the Database Software
@@ -10,8 +13,8 @@ import javax.swing.JFrame;
  */
 public class MainMenu extends JFrame{
     
-    JButton students;
-    JButton courses;
+    JButton students = new JButton("STUDENTS");
+    JButton courses = new JButton("COURSES");
     
     public MainMenu(){
         super("Database Menu");
@@ -21,6 +24,22 @@ public class MainMenu extends JFrame{
         this.setResizable(false);
         this.setSize(300,400);
         this.setLocationRelativeTo(null);
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4,1, 20, 10));
+        this.add(panel);
+        panel.setBounds(95, 150, 100, 200);
+        
+        JLabel label = new JLabel("Database Main Menu");
+        this.add(label);
+        label.setFont(new java.awt.Font("Unispace", 1, 18));
+        label.setBounds(40, 25, 225, 100);
+        
+        
+        panel.add(students);
+        panel.add(courses);
+        students.setSize(75, 30);
+        students.setSize(75, 30);
         
         this.setVisible(true);
         
